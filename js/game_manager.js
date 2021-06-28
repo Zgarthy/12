28e3fc0be73ef5e665627775cbf71c2570b0dea4
BombNumbers <78815731+BombNumbers@users.noreply.github.com>
2021-06-28 21:03:23 +0100

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -4,7 +4,7 @@ function GameManager(size, InputManager, Actuator, ScoreManager) {
   this.scoreManager = new ScoreManager;
   this.actuator     = new Actuator;
 
-  this.startTiles   = 1;
+  this.startTiles   = 64;
 
   this.inputManager.on("move", this.move.bind(this));
   this.inputManager.on("restart", this.restart.bind(this));
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.999998 ? Math.random() < 0.99998 ? Math.random() < 0.99995 ? Math.random() < 0.9999 ? Math.random() < 0.9998 ? Math.random() < 0.9995 ? Math.random() < 0.999 ? Math.random() < 0.995 ? 1 : 2 : 3 : 4 : 5 : 6 : 7 : 8 : 9;
+    var value = Math.random() < 0.99999 ? Math.random() < 0.99998 ? Math.random() < 0.99995 ? Math.random() < 0.9999 ? Math.random() < 0.9998 ? Math.random() < 0.9996 ? Math.random() < 0.999 ? Math.random() < 0.998 ? Math.random() < 0.99 ? 1 : 2 : 3 : 4 : 5 : 6 : 7 : 8 : 9 : 10;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
