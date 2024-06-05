e80f09a58f85f3e7cc77976595a697bd4863030b
Zgarthy <121491349+Zgarthy@users.noreply.github.com>
2024-06-05 19:07:46 +0100

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -69,6 +69,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   inner.classList.add("tile-inner");
   inner.textContent = tile.value;
   if (tile.value === 40) inner.textContent = "0";
+  if (tile.value === 37) inner.textContent = "🧊";
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
@@ -120,7 +121,7 @@ HTMLActuator.prototype.updateScore = function (score) {
   if (difference > 0) {
     var addition = document.createElement("div");
     addition.classList.add("score-addition");
-    addition.textContent = "+" + difference;
+    addition.textContent = "" + "";
 
     this.scoreContainer.appendChild(addition);
   }
